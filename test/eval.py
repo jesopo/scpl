542e4818ff1ba43a8a16b9e57de6ce3132d399f1
jesopo <github@lolnerd.net>
2021-12-20 17:03:37 +0000

    add IPv4 and IPv6 eval unit tests

--- test/eval.py
+++ test/eval.py
@@ -1,7 +1,10 @@
 import unittest
+from ipaddress import ip_network
+
 from scpl.lexer import tokenise
 from scpl.parser import parse
-from scpl.parser import ParseInteger, ParseFloat, ParseRegex, ParseString
+from scpl.parser import (ParseCIDRv4, ParseCIDRv6, ParseInteger, ParseFloat,
+    ParseRegex, ParseString)
 
 class EvalTestString(unittest.TestCase):
     def test_addstring(self):
@@ -16,3 +19,21 @@ class EvalTestString(unittest.TestCase):
         self.assertEqual(atom.pattern, "asd\.(?i:asd)")
         self.assertEqual(atom.delimiter, None)
         self.assertEqual(atom.flags, set())
+
+class EvalTestIPv4(unittest.TestCase):
+    def test_divideinteger(self):
+        addr = "10.84.1.1/24"
+        atom = parse(tokenise(addr), {})[0].eval({})
+        self.assertIsInstance(atom, ParseCIDRv4)
+        reference = ip_network(addr, strict=False)
+        self.assertEqual(atom.integer, int(reference.network_address))
+        self.assertEqual(atom.prefix, 24)
+
+class EvalTestIPv6(unittest.TestCase):
+    def test_divideinteger(self):
+        addr = "fd84:9d71:8b8:1::1/48"
+        atom = parse(tokenise(addr), {})[0].eval({})
+        self.assertIsInstance(atom, ParseCIDRv6)
+        reference = ip_network(addr, strict=False)
+        self.assertEqual(atom.integer, int(reference.network_address))
+        self.assertEqual(atom.prefix, 48)
