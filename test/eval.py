aee23508bfbbf1e56e4ae8ca9c6d57309bd9bae8
jesopo <github@lolnerd.net>
2021-12-21 12:01:58 +0000

    test invalid cidr prefixes

--- test/eval.py
+++ test/eval.py
@@ -29,11 +29,27 @@ class EvalTestIPv4(unittest.TestCase):
         self.assertEqual(atom.integer, int(reference.network_address))
         self.assertEqual(atom.prefix, 24)
 
+    def test_divideinteger_negative(self):
+        addr = "10.84.1.1/-1"
+        self.assertRaises(ValueError, lambda: parse(tokenise(addr), {})[0].eval({}))
+
+    def test_divideinteger_toobig(self):
+        addr = "10.84.1.1/33"
+        self.assertRaises(ValueError, lambda: parse(tokenise(addr), {})[0].eval({}))
+
 class EvalTestIPv6(unittest.TestCase):
-    def test_divideinteger(self):
+    def test_divideinteger_valid(self):
         addr = "fd84:9d71:8b8:1::1/48"
         atom = parse(tokenise(addr), {})[0].eval({})
         self.assertIsInstance(atom, ParseCIDRv6)
         reference = ip_network(addr, strict=False)
         self.assertEqual(atom.integer, int(reference.network_address))
         self.assertEqual(atom.prefix, 48)
+
+    def test_divideinteger_negative(self):
+        addr = "fd84:9d71:8b8:1::1/-1"
+        self.assertRaises(ValueError, lambda: parse(tokenise(addr), {})[0].eval({}))
+
+    def test_divideinteger_toobig(self):
+        addr = "fd84:9d71:8b8:1::1/129"
+        self.assertRaises(ValueError, lambda: parse(tokenise(addr), {})[0].eval({}))
