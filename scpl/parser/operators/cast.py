0182d693f41c0131c69667a840e8eef7e633f371
jesopo <github@lolnerd.net>
2021-12-21 12:28:40 +0000

    ParseCast* operators should inherit from ParseUnaryOperator

--- scpl/parser/operators/cast.py
+++ scpl/parser/operators/cast.py
@@ -1,17 +1,20 @@
 from re import escape as re_escape
 from typing import Dict
+from .common import ParseUnaryOperator
 from ..operands import ParseAtom, ParseFloat, ParseInteger, ParseRegex, ParseString
 
-class ParseCastIntegerFloat(ParseFloat):
+class ParseCastIntegerFloat(ParseUnaryOperator, ParseFloat):
     def __init__(self, atom: ParseInteger):
+        super().__init__(atom)
         self._atom = atom
     def __repr__(self) -> str:
         return f"CastFloat({self._atom!r})"
     def eval(self, variables: Dict[str, ParseAtom]) -> ParseFloat:
         return ParseFloat(self._atom.eval(variables).value)
 
-class ParseCastStringRegex(ParseRegex):
+class ParseCastStringRegex(ParseUnaryOperator, ParseRegex):
     def __init__(self, atom: ParseString):
+        super().__init__(atom)
         self._atom = atom
     def __repr__(self) -> str:
         return f"CastRegex({self._atom!r})"
