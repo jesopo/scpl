8ae3d4fe99d5e6b8b829d116f97ae48fac42b6d8
jesopo <github@lolnerd.net>
2021-12-23 17:09:35 +0000

    show how far lexing got for LexerUnfinishedError

--- scpl/lexer/__main__.py
+++ scpl/lexer/__main__.py
@@ -2,18 +2,23 @@ import sys
 from time   import monotonic
 from typing import Deque
 
-from .lexer  import tokenise, LexerError
+from .lexer  import tokenise, LexerError, LexerUnfinishedError
 from .tokens import Token
 
 def main_lexer(line: str) -> Deque[Token]:
     start = monotonic()
     try:
         tokens = tokenise(line)
-    except LexerError as e:
+    except LexerUnfinishedError as e1:
         print(line)
-        print(' '*e.index + "^")
-        print(str(e))
+        print(' '*e1.index + "^"*len(e1.token.text))
+        print(str(e1))
         sys.exit(1)
+    except LexerError as e2:
+        print(line)
+        print(' '*e2.index + "^")
+        print(str(e2))
+        sys.exit(2)
     else:
         end = monotonic()
         print(f"lexer   : {list(tokens)!r}")
