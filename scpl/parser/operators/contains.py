fb3547430d6d28373420e758378255a6c6130c16
jesopo <github@lolnerd.net>
2021-12-24 19:01:33 +0000

    implement Contains(str, str)

--- scpl/parser/operators/contains.py
+++ scpl/parser/operators/contains.py
@@ -1,7 +1,16 @@
 from typing import Dict, Optional
 from .common import ParseBinaryOperator
 from ..operands import (ParseAtom, ParseBool, ParseCIDR, ParseCIDRv4, ParseCIDRv6,
-    ParseIP, ParseIPv4, ParseIPv6)
+    ParseIP, ParseIPv4, ParseIPv6, ParseString)
+
+class ParseBinaryContainsStringString(ParseBinaryOperator, ParseBool):
+    def __init__(self, left: ParseString, right: ParseString):
+        self._left = left
+        self._right = right
+    def __repr__(self) -> str:
+        return f"Contains({self._left!r}, {self._right!r})"
+    def eval(self, vars: Dict[str, ParseAtom]) -> ParseBool:
+        return ParseBool(self._left.eval(vars).value in self._right.eval(vars).value)
 
 class ParseBinaryContainsIPCIDR(ParseBinaryOperator, ParseBool):
     def __init__(self, left: ParseIP, right: ParseCIDR):
@@ -16,15 +25,11 @@ class ParseBinaryContainsIPCIDR(ParseBinaryOperator, ParseBool):
         return ParseBool(network == right.integer)
 
 def find_binary_contains(left: ParseAtom, right: ParseAtom) -> Optional[ParseAtom]:
-    if isinstance(left, ParseIPv4):
-        if isinstance(right, ParseCIDRv4):
-            return ParseBinaryContainsIPCIDR(left, right)
-        else:
-            return None
-    if isinstance(left, ParseIPv6):
-        if isinstance(right, ParseCIDRv6):
-            return ParseBinaryContainsIPCIDR(left, right)
-        else:
-            return None
+    if isinstance(left, ParseString) and isinstance(right, ParseString):
+        return ParseBinaryContainsStringString(left, right)
+    elif isinstance(left, ParseIPv4) and isinstance(right, ParseCIDRv4):
+        return ParseBinaryContainsIPCIDR(left, right)
+    elif isinstance(left, ParseIPv6) and isinstance(right, ParseCIDRv6):
+        return ParseBinaryContainsIPCIDR(left, right)
     else:
         return None
