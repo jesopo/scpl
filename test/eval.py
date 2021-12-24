e0d68da6c0d01a005f805467df7b4acf8416c011
jesopo <github@lolnerd.net>
2021-12-24 19:02:49 +0000

    more string eval tests

--- test/eval.py
+++ test/eval.py
@@ -7,50 +7,49 @@ from scpl.parser import (ParseBool, ParseCIDRv4, ParseCIDRv6, ParseInteger, Pars
     ParseRegex, ParseString)
 
 class EvalTestString(unittest.TestCase):
-    def test_addstring(self):
+    def test_add_string(self):
         atom = parse(tokenise('"asd" + "asd"'), {})[0].eval({})
         self.assertIsInstance(atom, ParseString)
         self.assertEqual(atom.value, "asdasd")
         self.assertEqual(atom.delimiter, '"')
 
-    def test_addregex(self):
+    def test_not(self):
+        atom = parse(tokenise('!"asd"'), {})[0].eval({})
+        self.assertIsInstance(atom, ParseBool)
+        self.assertEqual(atom.value, False)
+        atom = parse(tokenise('!""'), {})[0].eval({})
+        self.assertEqual(atom.value, True)
+
+    def test_add_regex(self):
         atom = parse(tokenise('"asd." + /asd/i'), {})[0].eval({})
         self.assertIsInstance(atom, ParseRegex)
         self.assertEqual(atom.pattern, "asd\.(?i:asd)")
         self.assertEqual(atom.delimiter, None)
         self.assertEqual(atom.flags, set())
 
-    def test_equalstring(self):
+    def test_equal_string(self):
         atom = parse(tokenise('"asd" == "asd"'), {})[0].eval({})
         self.assertIsInstance(atom, ParseBool)
         self.assertEqual(atom.value, True)
-    def test_unequalstring(self):
-        atom = parse(tokenise('"asd" != "bsd"'), {})[0].eval({})
+        atom = parse(tokenise('"asd" == "bsd"'), {})[0].eval({})
+        self.assertEqual(atom.value, False)
+    def test_unequal_string(self):
+        atom = parse(tokenise('"asd" != "asd"'), {})[0].eval({})
         self.assertIsInstance(atom, ParseBool)
+        self.assertEqual(atom.value, False)
+        atom = parse(tokenise('"asd" != "bsd"'), {})[0].eval({})
         self.assertEqual(atom.value, True)
 
-class EvalTestIPv4(unittest.TestCase):
-    def test_divideinteger(self):
-        addr = "10.84.1.1/24"
-        atom = parse(tokenise(addr), {})[0].eval({})
-        self.assertIsInstance(atom, ParseCIDRv4)
-        reference = ip_network(addr, strict=False)
-        self.assertEqual(atom.integer, int(reference.network_address))
-        self.assertEqual(atom.prefix, 24)
+    def test_contains_string(self):
+        atom = parse(tokenise('"a" in "asd"'), {})[0].eval({})
+        self.assertIsInstance(atom, ParseBool)
+        self.assertEqual(atom.value, True)
+        atom = parse(tokenise('"b" in "asd"'), {})[0].eval({})
+        self.assertEqual(atom.value, False)
 
-    def test_divideinteger_toobig(self):
-        addr = "10.84.1.1/33"
-        self.assertRaises(ValueError, lambda: parse(tokenise(addr), {})[0].eval({}))
-
-class EvalTestIPv6(unittest.TestCase):
-    def test_divideinteger_valid(self):
-        addr = "fd84:9d71:8b8:1::1/48"
-        atom = parse(tokenise(addr), {})[0].eval({})
-        self.assertIsInstance(atom, ParseCIDRv6)
-        reference = ip_network(addr, strict=False)
-        self.assertEqual(atom.integer, int(reference.network_address))
-        self.assertEqual(atom.prefix, 48)
-
-    def test_divideinteger_toobig(self):
-        addr = "fd84:9d71:8b8:1::1/129"
-        self.assertRaises(ValueError, lambda: parse(tokenise(addr), {})[0].eval({}))
+    def test_match_regex(self):
+        atom = parse(tokenise('"asd" =~ /^asd$/'), {})[0].eval({})
+        self.assertIsInstance(atom, ParseString)
+        self.assertEqual(atom.value, "asd")
+        atom = parse(tokenise('"asd" =~ /^bsd$/'), {})[0].eval({})
+        self.assertEqual(atom.value, "")
