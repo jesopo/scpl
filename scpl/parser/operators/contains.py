df4aa9488bccb16eca99d9d9f5b832c53c75e820
jesopo <github@lolnerd.net>
2021-12-26 21:18:00 +0000

    parser: sets can be type agnostic given they're just about hashes

--- scpl/parser/operators/contains.py
+++ scpl/parser/operators/contains.py
@@ -2,7 +2,8 @@ from typing import Dict, Optional
 from .common import ParseBinaryOperator
 from ..operands import (ParseAtom, ParseBool, ParseCIDR, ParseCIDRv4, ParseCIDRv6,
     ParseIP, ParseIPv4, ParseIPv6, ParseString)
-from .set import ParseSetString
+from .set import ParseSet
+from .cast import find_cast_hash, ParseCastHash
 
 class ParseBinaryContainsStringString(ParseBinaryOperator, ParseBool):
     def __init__(self, left: ParseString, right: ParseString):
@@ -25,18 +26,24 @@ class ParseBinaryContainsIPCIDR(ParseBinaryOperator, ParseBool):
         network = self._left.eval(vars).integer & right.mask
         return ParseBool(network == right.integer)
 
-class ParseBinaryContainsStringSet(ParseBinaryOperator, ParseBool):
-    def __init__(self, left: ParseString, right: ParseSetString):
+class ParseBinaryContainsHashSet(ParseBinaryOperator, ParseBool):
+    def __init__(self, left: ParseCastHash, right: ParseSet):
         self._left = left
         self._right = right
     def __repr__(self) -> str:
         return f"Contains({self._left!r}, {self._right!r})"
     def eval(self, vars: Dict[str, ParseAtom]) -> ParseBool:
         right = self._right.eval(vars)
-        return ParseBool(self._left.eval(vars) in right)
+        return ParseBool(self._left.eval(vars).value in right)
 
 def find_binary_contains(left: ParseAtom, right: ParseAtom) -> Optional[ParseAtom]:
-    if isinstance(left, ParseString):
+    # check `right` first because if it is a set then we don't care what `left` is
+    if isinstance(right, ParseSet):
+        if (cast := find_cast_hash(left)) is not None:
+            return ParseBinaryContainsHashSet(cast, right)
+        else:
+            return None
+    elif isinstance(left, ParseString):
         if isinstance(right, ParseString):
             return ParseBinaryContainsStringString(left, right)
         elif isinstance(right, ParseSetString):
--- scpl/parser/operators/set.py
+++ scpl/parser/operators/set.py
@@ -7,40 +7,45 @@ class ParseSet(ParseAtom):
         self._base_atoms = atoms
     def __repr__(self) -> str:
         return f"Set({', '.join(repr(a) for a in self._base_atoms)})"
+    def _hashes(self) -> Sequence[int]:
+        raise NotImplementedError()
+    def eval(self, vars: Dict[str, ParseAtom]) -> Set[int]:
+        return set(self._hashes())
 
 class ParseSetInteger(ParseSet):
     def __init__(self, atoms: Sequence[ParseInteger]):
         super().__init__(atoms)
         self._atoms = atoms
-    def eval(self, vars: Dict[str, ParseAtom]) -> Set[ParseInteger]:
-        return set(atom.eval(vars) for atom in self._atoms)
+    def _hashes(self):
+        return [hash(a) for a in self._atoms]
 
 class ParseSetFloat(ParseSet):
     def __init__(self, atoms: Sequence[ParseFloat]):
         super().__init__(atoms)
         self._atoms = atoms
-    def eval(self, vars: Dict[str, ParseAtom]) -> Set[ParseFloat]:
-        return set(atom.eval(vars) for atom in self._atoms)
+    def _hashes(self):
+        return [hash(a) for a in self._atoms]
 
 class ParseSetString(ParseSet):
     def __init__(self, atoms: Sequence[ParseString]):
         super().__init__(atoms)
         self._atoms = atoms
-    def eval(self, vars: Dict[str, ParseAtom]) -> Set[ParseString]:
-        return set(atom.eval(vars) for atom in self._atoms)
+    def _hashes(self):
+        return [hash(a) for a in self._atoms]
 
 class ParseSetIPv4(ParseSet):
     def __init__(self, atoms: Sequence[ParseIPv4]):
         super().__init__(atoms)
         self._atoms = atoms
-    def eval(self, vars: Dict[str, ParseAtom]) -> Set[ParseIPv4]:
-        return set(atom.eval(vars) for atom in self._atoms)
+    def _hashes(self):
+        return [hash(a) for a in self._atoms]
+
 class ParseSetIPv6(ParseSet):
     def __init__(self, atoms: Sequence[ParseIPv6]):
         super().__init__(atoms)
         self._atoms = atoms
-    def eval(self, vars: Dict[str, ParseAtom]) -> Set[ParseIPv6]:
-        return set(atom.eval(vars) for atom in self._atoms)
+    def _hashes(self):
+        return [hash(a) for a in self._atoms]
 
 def _all_isinstance(atoms: Sequence[ParseAtom], atype: type) -> bool:
     for atom in atoms:
