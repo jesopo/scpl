1b437bc1da76703b2898ad2441d57f952e1eda20
jesopo <github@lolnerd.net>
2021-12-26 18:26:43 +0000

    parser: add bool variables

--- scpl/parser/operators/variable.py
+++ scpl/parser/operators/variable.py
@@ -1,5 +1,5 @@
 from typing import cast, Dict, Optional
-from ..operands import (ParseAtom, ParseFloat, ParseIPv4, ParseIPv6,
+from ..operands import (ParseAtom, ParseBool, ParseFloat, ParseIPv4, ParseIPv6,
     ParseInteger, ParseRegex, ParseString)
 
 class ParseVariable(ParseAtom):
@@ -14,23 +14,26 @@ class ParseVariable(ParseAtom):
         return False
 
 class ParseVariableString(ParseVariable, ParseString):
-    def eval(self, variables: Dict[str, ParseAtom]) -> ParseString:
-        return cast(ParseString, variables[self.name])
+    def eval(self, vars: Dict[str, ParseAtom]) -> ParseString:
+        return cast(ParseString, vars[self.name])
 class ParseVariableInteger(ParseVariable, ParseInteger):
-    def eval(self, variables: Dict[str, ParseAtom]) -> ParseInteger:
-        return cast(ParseInteger, variables[self.name])
+    def eval(self, vars: Dict[str, ParseAtom]) -> ParseInteger:
+        return cast(ParseInteger, vars[self.name])
 class ParseVariableFloat(ParseVariable, ParseFloat):
-    def eval(self, variables: Dict[str, ParseAtom]) -> ParseFloat:
-        return cast(ParseFloat, variables[self.name])
+    def eval(self, vars: Dict[str, ParseAtom]) -> ParseFloat:
+        return cast(ParseFloat, vars[self.name])
 class ParseVariableRegex(ParseVariable, ParseRegex):
-    def eval(self, variables: Dict[str, ParseAtom]) -> ParseRegex:
-        return cast(ParseRegex, variables[self.name])
+    def eval(self, vars: Dict[str, ParseAtom]) -> ParseRegex:
+        return cast(ParseRegex, vars[self.name])
+class ParseVariableBool(ParseVariable, ParseBool):
+    def eval(self, vars: Dict[str, ParseAtom]) -> ParseBool:
+        return cast(ParseBool, vars[self.name])
 class ParseVariableIPv4(ParseVariable, ParseIPv4):
-    def eval(self, variables: Dict[str, ParseAtom]) -> ParseIPv4:
-        return cast(ParseIPv4, variables[self.name])
+    def eval(self, vars: Dict[str, ParseAtom]) -> ParseIPv4:
+        return cast(ParseIPv4, vars[self.name])
 class ParseVariableIPv6(ParseVariable, ParseIPv6):
-    def eval(self, variables: Dict[str, ParseAtom]) -> ParseIPv6:
-        return cast(ParseIPv6, variables[self.name])
+    def eval(self, vars: Dict[str, ParseAtom]) -> ParseIPv6:
+        return cast(ParseIPv6, vars[self.name])
 
 def find_variable(name: str, var_type: type) -> Optional[ParseAtom]:
     if var_type == ParseString:
@@ -41,6 +44,8 @@ def find_variable(name: str, var_type: type) -> Optional[ParseAtom]:
         return ParseVariableFloat(name)
     elif var_type == ParseRegex:
         return ParseVariableRegex(name)
+    elif var_type == ParseBool:
+        return ParseVariableBool(name)
     elif var_type == ParseIPv4:
         return ParseVariableIPv4(name)
     elif var_type == ParseIPv6:
