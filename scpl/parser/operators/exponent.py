cd35493b4d0bfebbc9670b72ae05bdf916bf4990
jesopo <github@lolnerd.net>
2021-12-27 18:29:34 +0000

    parser: Exponent(int, Negative(int)) returns a float

--- scpl/parser/operators/exponent.py
+++ scpl/parser/operators/exponent.py
@@ -1,6 +1,7 @@
 from typing import Dict, Optional
 from .cast import ParseCastIntegerFloat
 from .common import ParseBinaryOperator
+from .negative import ParseUnaryNegativeFloat, ParseUnaryNegativeInteger
 from ..operands import ParseAtom, ParseInteger, ParseFloat
 
 class ParseBinaryExponentIntegerInteger(ParseBinaryOperator, ParseInteger):
@@ -28,16 +29,22 @@ class ParseBinaryExponentFloatInteger(ParseBinaryExponentFloatFloat):
 class ParseBinaryExponentIntegerFloat(ParseBinaryExponentFloatFloat):
     def __init__(self, left: ParseInteger, right: ParseFloat):
         super().__init__(ParseCastIntegerFloat(left), right)
+# Exponent(Integer, Negative(Integer)) produces a float
+class ParseBinaryExponentIntegerNegative(ParseBinaryExponentFloatFloat):
+    def __init__(self, left: ParseInteger, right: ParseUnaryNegativeInteger):
+        super().__init__(ParseCastIntegerFloat(left), ParseCastIntegerFloat(right))
 
 def find_binary_exponent(left: ParseAtom, right: ParseAtom) -> Optional[ParseAtom]:
     if isinstance(left, ParseInteger):
-        if isinstance(right, ParseInteger):
+        if isinstance(right, ParseUnaryNegativeInteger):
+            return ParseBinaryExponentIntegerNegative(left, right)
+        elif isinstance(right, ParseInteger):
             return ParseBinaryExponentIntegerInteger(left, right)
         elif isinstance(right, ParseFloat):
             return ParseBinaryExponentIntegerFloat(left, right)
         else:
             return None
-    if isinstance(left, ParseFloat):
+    elif isinstance(left, ParseFloat):
         if isinstance(right, ParseFloat):
             return ParseBinaryExponentFloatFloat(left, right)
         elif isinstance(right, ParseInteger):
